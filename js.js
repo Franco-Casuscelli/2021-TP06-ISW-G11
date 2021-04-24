7f966cdd59b7645fb39ddba1cf9484d6f752617d
Javiermanzano73261 <manzanojavier@hotmail.com>
2021-04-24 13:12:17 -0300

    Forma de pago

---
+++ js.js
@@ -0,0 +1,52 @@
+var select = document.getElementById("select"),
+                     arr = [
+                        "Almafuerte","Alta gracia","Arroyito","Bell ville","Capilla del monte", 
+                        "Colonia caroya","Cosquín","Cruz del eje","Córdoba","Deán funes","Estación",
+                        "General cabrera","General deheza","Jesús maría","Juárez celman",
+                        "La calera","La carlota","La falda","Laboulaye","Las varillas","Malagueño",
+                        "Malvinas argentinas","Marcos juárez","Morteros","Oliva","Oncativo","Pilar",
+                        "Río ceballos","Río cuarto","Río segundo","Río tercero","Saldán","San francisco",
+                        "Santa rosa de calamuchita","Unquillo","Villa allende","Villa carlos paz",
+                        "Villa del rosario","Villa dolores","Villa maría","Villa nueva"
+                        ];
+             
+             for(var i = 0; i < arr.length; i++)
+             {
+                 var option = document.createElement("OPTION"),
+                     txt = document.createTextNode(arr[i]);
+                 option.appendChild(txt);
+                 option.setAttribute("value",arr[i]);
+                 select.insertBefore(option,select.lastChild);
+             }
+             var element = document.querySelector('select');
+
+             element.addEventListener('mousedown', function () {
+                 this.size=8;
+             });
+             element.addEventListener('change', function () {
+                 this.blur();
+             });
+             element.addEventListener('blur', function () {
+                 this.size=0;
+             });
+
+             // para la segunda opcion lo unico que hay q hacer es quitar desde la linea 21 a la 31
+
+             //Deshabitilar y Habilitar input radio button 
+            
+             function deshabilitarInput(){
+             if(document.querySelector('#hora_pedido').value !== ''){
+                 document.querySelector('#btn_antes_posible').disabled = true;
+             }
+             else{
+                document.querySelector('#btn_antes_posible').disabled = false;
+                if(document.querySelector('#btn_antes_posible').checked === true){
+                    document.querySelector('#hora_pedido').disabled = true;
+                    document.querySelector('#hora_pedido').value = '';
+                }
+                else{
+                    document.querySelector('#hora_pedido').disabled = false;
+                    
+                }
+             }
+            }
