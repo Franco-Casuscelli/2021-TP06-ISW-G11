5bdb43ce94b1c3a8840efb6dff3d78e3bfef6514
Franco Diaz Luna <diazlunafrancoe@gmail.com>
2021-04-26 19:09:32 -0300

    se agrega dir de comercio + localizacion

--- js.js
+++ js.js
@@ -289,3 +289,98 @@ function validar(){
     alert("llegue!!")
 
 }
+
+
+
+// G-Maps ---------------------------------------------------------
+
+// Ver coord de ubicación en consola 
+//var ubicacion = new Localizacion();
+//console.log(ubicacion)
+google.maps.event.addDomListener(window, "load", function(){
+    const ubicacion = new Localizacion(()=>{
+        const myLatLng = {
+                lat : ubicacion.latitude,
+                lng : ubicacion.longitude
+        }
+
+        //var texto = '<h4>Tu ubicación actual</h4>' + '<br><p>Aquí te encuentras actualmente</p>'
+
+        const options = {
+            center: myLatLng,
+            zoom: 16
+        }
+
+        var map = document.getElementById('map');
+
+        const mapa = new google.maps.Map(map,options);
+
+        const marcador = new google.maps.Marker({
+            position: myLatLng,
+            map: mapa
+            //title: "Marcador"
+            
+        })
+
+        var informacion = new google.maps.InfoWindow({
+            //content: texto
+        })
+
+        marcador.addListener('click', function(){
+            informacion.open(mapa,marcador);
+        });
+
+        var autocoplete = document.getElementById('txt_comercio');
+
+        const search = new google.maps.places.Autocomplete(autocoplete);
+        search.bindTo("bounds",mapa);
+
+        search.addListener('place_changed', function(){
+            informacion.close();
+            marcador.setVisible(false);
+
+            var place = search.getPlace();
+
+            if(!place.geometry.viewport){
+                window.alert("Error al mostrar el lugar");
+                return
+            }
+            
+            if(place.geometry.viewport){
+                mapa.fitBounds(palce.geometry.viewport);
+            }else{
+                map.setCenter(palce.geometry.location);
+                mapa.setZoom(18);
+            }
+
+            marcador.setPositiom(place.geometry.location);
+            marcador.setVisible(true);
+
+            var address = ""
+
+            if(palce.address_components){
+                address = [
+                    (place.address_components[0] && place.address_components[0].short_name || ''),
+                    (place.address_components[1] && place.address_components[1].short_name || ''),
+                    (place.address_components[2] && place.address_components[2].short_name || '')
+                ];
+            }
+
+            informacion.setContenet('<div><strong>'+place.name+'</strong><br>'+address+'</div>');
+            informacion.open(map,marcador);
+
+        });
+
+    });
+
+});
+
+// G-Maps ---------------------------------------------------------
+
+function ocultar(){
+    document.getElementById('dirComercio').style.display = 'none';
+}
+
+function mostrar(){
+    document.getElementById('map').style.display = true;
+}
