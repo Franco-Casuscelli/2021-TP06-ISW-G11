50393d71394182c2b5b22d01fbaa2bd7e724c67e
Franco Diaz Luna <diazlunafrancoe@gmail.com>
2021-04-26 23:07:41 -0300

    direccion comercio + mapas

--- js.js
+++ js.js
@@ -7,7 +7,7 @@ $( document ).ready(function() {
     document.getElementById("error_txt_solo_imagenes").style.display = 'none';
     document.getElementById("error_txt_5_megas").style.display = 'none';
     document.getElementById("error_txt_recibirlo").style.display = 'none';
-    
+    document.getElementById('map').style.display = 'none';
 });
 
 var pagoSeleccionado;
@@ -195,17 +195,174 @@ var select = document.getElementById("select"),
             });
         }
     })
+*/
 
-    
-    /* Nahuel*/
 
+
+//--- Validaciones comercio
+const formularioComercio = document.getElementById('formulario');
+const inputComercio = document.querySelectorAll('#formulario input');
+const camposComercio = {
+    calleComercio: false,
+    numeroComercio: false,
+    ciudadComercio: false
+}
+const expresionesComercio = {
+    calleComercio: /^[a-zA-Z0-9\s]{3,30}$/, // Letras, numeros, guion y guion_bajo
+    ciudadComercio: /^[a-zA-ZÀ-ÿ\s]{3,20}$/, // Letras y espacios, pueden llevar acentos.
+    numeroComercio: /^\d{1,5}$/ // 1 a 5 numeros.
+}
+
+const validarFormularioComercio = (e) => {
+    switch(e.target.name) {
+        case "calleComercio":
+            validarCampoComercio(expresionesComercio.calleComercio, e.target, 'calle_comercio');
+        break;
+
+        case "numeroComercio":
+            validarCampoComercio(expresionesComercio.numeroComercio, e.target, 'numero_comercio');
+        break;
+
+        case "ciudadComercio":
+            validarCampoComercio(expresionesComercio.ciudadComercio, e.target, 'ciudad_comercio');
+        break;
+    }
+}
+
+const validarCampoComercio = (expresion, input, campo) =>{
+    if(expresion.test(input.value)){
+        document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+        document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+        document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+        camposComercio[campo] = true;
+    }
+    else{
+        document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+        document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+        document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+        camposComercio[campo] = false;
+    }
+}
+
+inputComercio.forEach((input) => {
+    input.addEventListener('keyup', validarFormularioComercio);
+    input.addEventListener('blur', validarFormularioComercio);
+});
+/*
+function tomarDatosDomicilioComercio(){
+    const completo = "Dirección de comercio";
+    const calle_ = document.getElementById("calleComercio").value;
+    const numero_ = document.getElementById("numeroComercio").value;
+    const ciudad_ = document.getElementById("ciudadComercio").value;
+
+    completo = calle_ + " " + numero_ + " " + ciudad_;
+
+    document.write(completo);
+}*/
+//-------------------------
+
+
+// G-Maps ---------------------------------------------------------
+
+// Ver coord de ubicación en consola 
+//var ubicacion = new Localizacion();
+//console.log(ubicacion)
+google.maps.event.addDomListener(window, "load", function(){
+    const ubicacion = new Localizacion(()=>{
+        const myLatLng = {
+                lat : ubicacion.latitude,
+                lng : ubicacion.longitude
+        }
+
+        //var texto = '<h4>Tu ubicación actual</h4>' + '<br><p>Aquí te encuentras actualmente</p>'
+
+        const options = {
+            center: myLatLng,
+            zoom: 16
+        }
+
+        var map = document.getElementById('map');
+
+        const mapa = new google.maps.Map(map,options);
+
+        const marcador = new google.maps.Marker({
+            position: myLatLng,
+            map: mapa
+            //title: "Marcador"
+            
+        })
+
+        var informacion = new google.maps.InfoWindow({
+            //content: texto
+        })
+
+        marcador.addListener('click', function(){
+            informacion.open(mapa,marcador);
+        });
+
+        var autocoplete = document.getElementById('txt_comercio');
+
+        const search = new google.maps.places.Autocomplete(autocoplete);
+        search.bindTo("bounds",mapa);
+
+        search.addListener('place_changed', function(){
+            informacion.close();
+            marcador.setVisible(false);
+
+            var place = search.getPlace();
+
+            if(!place.geometry.viewport){
+                window.alert("Error al mostrar el lugar");
+                return
+            }
+            
+            if(place.geometry.viewport){
+                mapa.fitBounds(palce.geometry.viewport);
+            }else{
+                map.setCenter(palce.geometry.location);
+                mapa.setZoom(18);
+            }
+
+            marcador.setPositiom(place.geometry.location);
+            marcador.setVisible(true);
+
+            var address = ""
+
+            if(palce.address_components){
+                address = [
+                    (place.address_components[0] && place.address_components[0].short_name || ''),
+                    (place.address_components[1] && place.address_components[1].short_name || ''),
+                    (place.address_components[2] && place.address_components[2].short_name || '')
+                ];
+            }
+
+            informacion.setContenet('<div><strong>'+place.name+'</strong><br>'+address+'</div>');
+            informacion.open(map,marcador);
+
+        });
+
+    });
+
+});
+
+// G-Maps ---------------------------------------------------------
+
+function ubicacionActual(){
+    document.getElementById('datos_comercio').style.display = 'none';
+    document.getElementById('map').style.display = 'block';
+}
+
+function ubicacionEspecifica(){
+    document.getElementById('datos_comercio').style.display = 'block';
+    document.getElementById('map').style.display = 'none';
+}
 
     
 
 function validar(){
     
     var txt_pedido = document.getElementById('txt_pedido').value;
-    var txt_comercio = document.getElementById('txt_comercio').value;
+    //var txt_comercio = document.getElementById('txt_comercio').value;
     var fecha_pedido = document.getElementById('fecha_pedido').value;
     //var txt_direccion_entrega = document.getElementById('txt_direccion_entrega').value;
     var select = document.getElementById('select').value;
@@ -220,11 +377,23 @@ function validar(){
         document.getElementById("error_txt_recibirlo").style.display = 'none';
         return
     }
+    /*
     if(txt_comercio == ''){
         document.getElementById("error_txt_comercio").style.display = 'block';
 
         document.getElementById("error_txt_pedido").style.display = 'none';
         document.getElementById("error_txt_entrega").style.display = 'none';
+        document.getElementById("error_txt_ciudad").style.display = 'none';
+        document.getElementById("error_pagoElegido").style.display = 'none';
+        document.getElementById("error_txt_recibirlo").style.display = 'none';
+        return
+    }
+    */
+
+    if(campos.calleComercio == false || campos.numeroComercio == false || campos.ciudadComercio == false){
+        document.getElementById("error_txt_entrega").style.display = 'none';
+        document.getElementById("error_txt_comercio").style.display = 'block';
+        document.getElementById("error_txt_pedido").style.display = 'none';
         document.getElementById("error_txt_ciudad").style.display = 'none';
         document.getElementById("error_pagoElegido").style.display = 'none';
         document.getElementById("error_txt_recibirlo").style.display = 'none';
@@ -319,7 +488,7 @@ function validar(){
 
     document.getElementById("error_txt_recibirlo").style.display = 'none';
     document.getElementById("error_txt_pedido").style.display = 'none';
-    document.getElementById("error_txt_comercio").style.display = 'none';
+    Sdocument.getElementById("error_txt_comercio").style.display = 'none';
     document.getElementById("error_txt_entrega").style.display = 'none';
     document.getElementById("error_txt_ciudad").style.display = 'none';
 
